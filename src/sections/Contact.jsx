1c945ca754d0fb2aea8b6690385122d7083911f7
capivman <knightdeath9999@gmail.com>
2026-07-25 12:16:54 -0300

    chore: finalize MVP

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,11 @@
 function Contact() {
+  const phone = import.meta.env.VITE_WHATSAPP_NUMBER;
+
+  const message =
+    "Olá! Adorei seu portfolio e gostaria de mais informações de como posso contratá-la como fotógrafa";
+
+  const whatsappUrl = `https://api.whatsapp.com/send?phone=${phone}&text=${encodeURIComponent(message)}`;
+
   return (
     <section
       id="contact"
@@ -14,31 +21,31 @@ function Contact() {
         Então me chama no WhatsApp para transformarmos suas ideias em memórias inesquecíveis.
       </p>
 
-<a
-  href="https://wa.me/55SEUNUMERO"
-  target="_blank"
-  rel="noreferrer"
-  className="
-    title-font
-    rounded-xl
-    border-3
-    border-[#25D366]
-    bg-[#25D366]/10
-    px-8
-    py-4
-    text-2xl
-    text-white
-    shadow-[0_0_18px_rgba(37,211,102,.8)]
-    transition-all
-    duration-300
-    hover:scale-105
-    hover:bg-[#25D366]
-    hover:shadow-[0_0_30px_rgba(37,211,102,.6)]
-    active:scale-95
-  "
->
-  WhatsApp
-</a>
+      <a
+        href={whatsappUrl}
+        target="_blank"
+        rel="noreferrer"
+        className="
+          title-font
+          rounded-xl
+          border-3
+          border-[#25D366]
+          bg-[#25D366]/10
+          px-8
+          py-4
+          text-2xl
+          text-white
+          shadow-[0_0_18px_rgba(37,211,102,.8)]
+          transition-all
+          duration-300
+          hover:scale-105
+          hover:bg-[#25D366]
+          hover:shadow-[0_0_30px_rgba(37,211,102,.6)]
+          active:scale-95
+        "
+      >
+        WhatsApp
+      </a>
     </section>
   );
 }
